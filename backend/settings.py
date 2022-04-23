45c8354fa3a9b1fd7ba2832082c9cecb50da1140
Nikolay Trafimenkov <xml12333@gmail.com>
2022-04-23 15:48:48 +0300

    add template

--- backend/settings.py
+++ backend/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 from .local_settings import (SECRET_KEY, DEBUG,DATABASES)
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'pages.apps.PagesConfig'
+    'pages.apps.PagesConfig',
 ]
 
 MIDDLEWARE = [
@@ -113,7 +114,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+
 STATIC_URL = '/static/'
+STATIC_ROOT =  os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'backend/static'),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
--- pages/views.py
+++ pages/views.py
@@ -2,3 +2,16 @@ from django.shortcuts import render
 
 def home(request):
     return render(request,'pages/home.html')
+
+def about(request):
+    return render(request, 'pages/about.html')
+
+def services(request):
+    return render(request, 'pages/services.html')
+    
+def contact(request):
+    return render(request, 'pages/contact.html')
+
+def cars(request):
+    return render(request, 'pages/cars.html')
+    
